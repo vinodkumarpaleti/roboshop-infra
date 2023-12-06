6bfa999e729e9b900573e25c132010feac1321ae
vinodkumar <kumarvinod.paleti@gmail.com>
2023-12-06 21:00:01 +0530

    new files are added

--- variables.tf
+++ variables.tf
@@ -15,3 +15,55 @@ variable "vpc_tags" {
     Name = "roboshop"
   }
 }
+
+variable "internet_gateway_tags" {
+
+  default = {
+    Name = "roboshop"
+  }
+  
+}
+variable "public_subnet_cidr" {
+  default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+
+variable "azs" {
+  default = ["us-east-1a","us-east-1b"]
+}
+
+variable "public_subnet_names" {
+  default = ["roboshop-public-1a","roboshop-public-1b"]
+}
+
+variable "private_subnet_cidr" {
+  default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+variable "private_subnet_names" {
+  default = ["roboshop-private-1a","roboshop-private-1b"]
+}
+
+variable "database_subnet_cidr" {
+  default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
+variable "database_subnet_names" {
+  default = ["roboshop-database-1a","roboshop-database-1b"]
+}
+
+variable "public_route_table_tags" {
+  default = {
+    Name = "roboshop-public"
+  }
+}
+
+variable "private_route_table_tags" {
+  default = {
+    Name = "roboshop-private"
+  }
+}
+
+variable "database_route_table_tags" {
+  default = {
+    Name = "roboshop-database"
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -3,4 +3,23 @@ module "this" {
   cidr_block = var.cidr_block
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
+  igw_tags = var.internet_gateway_tags
+  
+  # public subnet creation  
+  public_subnet_cidr = var.public_subnet_cidr
+  azs = var.azs
+  public_subnet_names = var.public_subnet_names
+
+  # private subnet creation
+  private_subnet_cidr = var.private_subnet_cidr
+  private_subnet_names = var.private_subnet_names
+
+  # database subnet creation
+  database_subnet_cidr = var.database_subnet_cidr
+  database_subnet_names = var.database_subnet_names
+
+   #public route table
+  public_route_table_tags = var.public_route_table_tags
+  private_route_table_tags = var.private_route_table_tags
+  database_route_table_tags = var.database_route_table_tags
 }
